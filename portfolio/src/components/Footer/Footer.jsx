0a21d6277520e22384bd441fafa165f18c59fd94
ChelseaBlaze <81193778+ChelseaBlaze@users.noreply.github.com>
2022-05-13 00:52:24 +0200

    added video and frame to about. no sound.

--- portfolio/src/components/Footer/Footer.jsx
+++ portfolio/src/components/Footer/Footer.jsx
@@ -7,13 +7,13 @@ import { AiFillInstagram,
 const Footer = () => {
   return (
     <footer className="flex flex-row w-full h-36 bg-black text-white fixed left-0 right-0 bottom-0 items-center justify-center ">
-      <a href="https://www.instagram.com/chel.css" target="_blank" className=" transition duration-500 hover:text-main1 ">
+      <a href="https://www.instagram.com/chel.css" target="_blank" className=" transition duration-700 hover:text-main1 ">
         <AiFillInstagram className=" w-16 h-16" alt="instagram"/>
       </a>
-      <a href="https://github.com/ChelseaBlaze" target="_blank" className=" transition duration-500 hover:text-main1 ">
+      <a href="https://github.com/ChelseaBlaze" target="_blank" className=" transition duration-700 hover:text-main1 ">
         <AiOutlineGithub className=" w-16 h-16 mx-5 " alt="github"/>
       </a>
-      <a href="https://www.linkedin.com/in/chelseatrimmer/" target="_blank" className=" transition duration-500 hover:text-main1 ">
+      <a href="https://www.linkedin.com/in/chelseatrimmer/" target="_blank" className=" transition duration-700 hover:text-main1 ">
         <AiFillLinkedin className=" w-16 h-16 " alt="linkedin"/>
       </a>
       
--- portfolio/src/pages/About.jsx
+++ portfolio/src/pages/About.jsx
@@ -1,9 +1,24 @@
+import { Footer } from "../components";
+import  Phone  from "../assets/iphone.svg"
+import Closing from "../assets/closing.mp4"
+
 const About = () => {
 
     return (
-    <section>
-
-    </section>
+      <main className = " bg-black w-full min-h-screen ">
+        <section className=" about_container flex w-full h-4/5 justify-center align-center self-center pt-24">
+          <section className=" w-2/5 flex ">
+            text box
+          </section>
+          <section className=" w-2/4 relative align-center">
+              <img className="object-cover absolute z-10" src={Phone} alt="iPhone svg" />
+              <video className=" w-3/4 absolute z-11 ml-24 mt-10 " autoPlay muted >
+                <source src = {Closing} type="video/mp4" />
+              </video>
+          </section>
+        </section>
+      <Footer />
+  </main>
       )
   };
   
