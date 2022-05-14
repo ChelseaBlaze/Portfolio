b21d94c94013820eeadf11435dbe88b8975bfc14
ChelseaBlaze <81193778+ChelseaBlaze@users.noreply.github.com>
2022-05-14 22:47:24 +0200

    linked projects

--- portfolio/src/pages/Projects.jsx
+++ portfolio/src/pages/Projects.jsx
@@ -20,7 +20,9 @@
 
               <section className=" flex flex-col md:flex-row w-full h-4/5 justify-between align-center self-center pt-24">
                 <section className=" flex self-center justify-center items-center w-full md:w-2/5 z-1 ">
-                    <img src={Happeningdemo} className="w-1/2" />
+                    <a href="https://benciantar-happening.netlify.app/" className="w-1/2">
+                    <img src={Happeningdemo} />
+                    </a>
                 </section>
                 <section className=" flex self-center justify-center items-center w-11/12 md:w-3/5 text-black -mt-5 md:-mt-0">
                   <img src= {Projectex} alt="An e-commerce website called Happening, that sells experiences as gifts. Texts reads: " className=" w-11/12 md:w-5/6" />
@@ -29,10 +31,12 @@
 
               <section className=" flex flex-col-reverse md:flex-row w-full h-4/5 justify-between align-center self-center pt-24">
                 <section className=" flex self-center justify-center items-center w-11/12 md:w-2/4 text-black -mt-5 md:-mt-0">
-                  <img src= {Projectex} alt="A web site for a fictitious island called Isla Tranquila. Texts reads: " className="w-11/12" />
+                  <img src= {Projectex} alt="A web game called a Florida Man, built in phaser.js Texts reads: " className="w-11/12" />
                 </section>
                 <section className=" flex self-center justify-center items-center w-full md:w-2/4 md:mr-6 ">
-                    <img src={Floridamandemo} className="w-11/12" />
+                    <a href="https://chelseablaze.github.io/florida-game/" className="w-11/12">
+                    <img src={Floridamandemo} />
+                    </a>
                 </section>
               </section>
 
