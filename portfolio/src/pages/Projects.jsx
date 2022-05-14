1e591a92d0a9a5bdf8560d386bac2229ce180e3b
ChelseaBlaze <81193778+ChelseaBlaze@users.noreply.github.com>
2022-05-14 20:44:14 +0200

    added all projects

--- portfolio/src/pages/Projects.jsx
+++ portfolio/src/pages/Projects.jsx
@@ -1,6 +1,7 @@
-      import { Footer } from "../components";
       import Projectex from "../assets/project.png"
       import Islademo from "../assets/islatranquila.svg"
+      import Happeningdemo from "../assets/happening.svg"
+      import Floridamandemo from "../assets/floridaman.svg"
 
       
       const Projects = () => {
@@ -16,6 +17,25 @@
                     <img src={Islademo} className="w-11/12" />
                 </section>
               </section>
+
+              <section className=" flex flex-col md:flex-row w-full h-4/5 justify-between align-center self-center pt-24">
+                <section className=" flex self-center justify-center items-center w-full md:w-2/5 z-1 ">
+                    <img src={Happeningdemo} className="w-1/2" />
+                </section>
+                <section className=" flex self-center justify-center items-center w-11/12 md:w-3/5 text-black -mt-5 md:-mt-0">
+                  <img src= {Projectex} alt="An e-commerce website called Happening, that sells experiences as gifts. Texts reads: " className=" w-11/12 md:w-5/6" />
+                </section>
+              </section>
+
+              <section className=" flex flex-col-reverse md:flex-row w-full h-4/5 justify-between align-center self-center pt-24">
+                <section className=" flex self-center justify-center items-center w-11/12 md:w-2/4 text-black -mt-5 md:-mt-0">
+                  <img src= {Projectex} alt="A web site for a fictitious island called Isla Tranquila. Texts reads: " className="w-11/12" />
+                </section>
+                <section className=" flex self-center justify-center items-center w-full md:w-2/4 md:mr-6 ">
+                    <img src={Floridamandemo} className="w-11/12" />
+                </section>
+              </section>
+
         </main>
             )
         };
