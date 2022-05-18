4127a7c5e42be453a130ba1d60ca835fa1aa7263
ChelseaBlaze <81193778+ChelseaBlaze@users.noreply.github.com>
2022-05-18 13:46:16 +0200

    changed logo and resized

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from "react"; 
 import { NavLink } from "react-router-dom";
+import Logo from "../assets/homelogo.svg";
+
+
 /*
 import {
   AiOutlineMenu,
@@ -10,19 +13,19 @@ import {
 const Navbar = () => {
 
   return (
-  <nav className =" navbar flex width-full h-20 bg-black text-white md:text-2xl lg:text-3xl text-base">
-      <section className = " logo flex justify-start ml-1 md:ml-6 items-center w-1/4 ">
-            <NavLink to="/">
-                <p className=" mx-3 md:mx-6 transition duration-500 hover:text-main1">Logo</p>
+  <nav className =" navbar flex width-full h-20 bg-black text-white md:text-2xl lg:text-3xl text-xl">
+      <section className = " logo flex justify-start ml-1 self-center items-center w-1/4">
+            <NavLink to="/" className="w-3/4 md:w-1/3 " >
+                <img src={Logo} className=" mx-2 md:mx-3 "/>
             </NavLink>
       </section>
       <section className = " navmenu flex flex-row justify-end items-center w-3/4 mr-2 md:mr-6 ">
         <ul className = " linklist flex flex-row  ">
             <NavLink to="/Projects">
-                <li className=" mx-3 md:mx-8transition duration-500 hover:text-main1">Projects</li>
+                <li className=" mx-3 md:mx-8 transition duration-500 hover:text-main1">Projects</li>
             </NavLink>
             <NavLink to="/About">
-                <li className=" mx-3 md:mx-8 transition duration-500 hover:text-main1 ">About Me</li>
+                <li className=" mx-2 md:mx-8 transition duration-500 hover:text-main1 ">About Me</li>
             </NavLink>
         </ul>  
         
