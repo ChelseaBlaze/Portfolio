f440888cb9908cbd44c0087d02173e0a741505a3
ChelseaBlaze <114475432+Chelsea-Blaze@users.noreply.github.com>
2023-09-15 11:56:13 +0200

    re-sized text boxes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,10 @@ import mobilepic from "../assets/mobilepic.png"
 const Home = () => {
 
     return (
-    <main className = "bg-black w-full h-screen ">
-        <section className="self-center w-full h-4/5 ">
-            <img className="hidden md:block md:w-11/12 lg:w-5/6 pl-16 lg:pl-40 pt-32 lg:pt-10 max-w-screen-xl " src={mainpic} alt="" />
-            <img className="w-5/6 self-center mx-6 pt-24 md:hidden" src={mobilepic} alt="" />
+    <main className = "bg-black w-full min-h-screen flex lg:m-0 ">
+        <section className=" self-center w-screen">
+            <img className="hidden md:block md:w-11/12 lg:flex lg:w-5/6 pl-16 lg:pl-40 lg:m-0    " src={mainpic} alt="" />
+            <img className="w-5/6 self-center mx-6  md:hidden" src={mobilepic} alt="" />
         </section>
         
     </main>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -22,13 +22,13 @@
                     </a>
                   </section>
                   <section className=" flex self-center  items-center w-11/12 md:w-3/5 text-black -mt-5 md:-mt-0">
-                   <img src= {Twineinfo} alt="Twine is a digital tool that seamlessly integrates multiple HR platforms" className=" w-full md:w-5/6" />
+                   <img src= {Twineinfo} alt="Twine is a digital tool that seamlessly integrates multiple HR platforms" className=" w-full lg:w-4/6 md:w-5/6" />
                   </section>
               </section>
 
               <section className=" flex flex-col-reverse md:flex-row w-full h-4/5 justify-between align-center self-center pt-12 ">
                 <section className=" flex self-center justify-center items-center w-11/12 md:w-2/4 text-black -mt-5 md:-mt-0">
-                  <img src= {Floridamaninfo} alt="A web game called a Florida Man, built in phaser.js Texts reads: " className="w-full lg:w-11/12" />
+                  <img src= {Floridamaninfo} alt="A web game called a Florida Man, built in phaser.js Texts reads: " className="w-full lg:w-10/12" />
                 </section>
                 <section className=" flex self-center justify-center items-center w-full md:w-2/4 md:mr-6 ">
                     <a href="https://chelseablaze.github.io/florida-game/" target="_blank" className="w-11/12">
@@ -44,13 +44,13 @@
                     </a>
                 </section>
                 <section className=" flex self-center  items-center w-11/12 md:w-3/5 text-black -mt-5 md:-mt-0">
-                  <img src= {Happeninginfo} alt="An e-commerce website called Happening, that sells experiences as gifts. Texts reads: " className=" w-full md:w-5/6" />
+                  <img src= {Happeninginfo} alt="An e-commerce website called Happening, that sells experiences as gifts. Texts reads: " className=" w-full lg:w-4/6 md:w-5/6" />
                 </section>
               </section>
 
               <section className=" flex flex-col-reverse md:flex-row w-full h-4/5 justify-between align-center self-center pt-24">
                 <section className=" flex self-center justify-center items-center w-11/12 md:w-2/4 text-black -mt-5 md:-mt-0 ">
-                  <img src= {Islainfo} alt="A web site for a fictitious island called Isla Tranquila. Texts reads: " className="w-full lg:w-11/12" />
+                  <img src= {Islainfo} alt="A web site for a fictitious island called Isla Tranquila. Texts reads: " className="w-full lg:w-10/12" />
                 </section>
                 <section className=" flex self-center justify-center items-center w-full md:w-2/4 md:mr-6 ">
                   <a href="https://isla-tranquila.netlify.app/" target="_blank" className="w-11/12">
